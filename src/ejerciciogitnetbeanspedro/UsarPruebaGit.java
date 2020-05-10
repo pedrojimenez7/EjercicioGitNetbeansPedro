573000852422515ff52d9593a6ce08e96b6e7862
Pedro <jimenezmartinpedro@gmail.com>
2020-05-10 18:04:51 +0200

    Añadida la clase UsarPruebaGit

---
+++ src/ejerciciogitnetbeanspedro/UsarPruebaGit.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciogitnetbeanspedro;
+
+/**
+ *
+ * @author Pedro
+ */
+public class UsarPruebaGit {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde Git");
+        PruebaGit prueba1 = new PruebaGit();
+        prueba1.numero= 20;
+        System.out.println(prueba1.numero);
+    }
+}
