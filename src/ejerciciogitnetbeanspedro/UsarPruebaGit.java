ac851c829e8735f377a8eeb2c02de2bbe39234cf
pedrojimenez7 <65169109+pedrojimenez7@users.noreply.github.com>
2020-05-11 14:39:01 +0200

    Nuevo mensaje para UsarPruebaGit

--- src/ejerciciogitnetbeanspedro/UsarPruebaGit.java
+++ src/ejerciciogitnetbeanspedro/UsarPruebaGit.java
@@ -11,7 +11,7 @@ package ejerciciogitnetbeanspedro;
  */
 public class UsarPruebaGit {
     public static void main(String[] args) {
-        System.out.println("Hola mundo desde Git");
+        System.out.println("Hola mundo desde GitHub");
         PruebaGit prueba1 = new PruebaGit();
         prueba1.numero= 20;
         System.out.println(prueba1.numero);
